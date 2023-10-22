7efb007541142532f6a3792a7d7e6ee7d027ac13
Ankur Seth <iankurseth@gmail.com>
2023-10-22 13:09:34 +0530

    Added first page: ui/text

---
+++ next-ui-redpapr/app/ui/text/page.tsx
@@ -0,0 +1,12 @@
+import { H1, H2 } from "../../../../package/src";
+
+
+
+export default function Text() {
+	return (
+		<header className="">
+			<H1>This is an H1</H1>
+			<H2>This is an H2</H2>
+		</header>
+	);
+}
