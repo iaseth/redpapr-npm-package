430d6437211216802a593b4bc4bc6f669296da55
Ankur Seth <iankurseth@gmail.com>
2023-10-23 15:00:30 +0530

    Updated homepage

--- next-ui-redpapr/app/page.tsx
+++ next-ui-redpapr/app/page.tsx
@@ -1,10 +1,16 @@
 
 
 
+export const metadata = {
+	title: 'Redpapr UI',
+	description: 'Documentation for the Redpapr UI Library.',
+};
+
 export default function Home() {
 	return (
 		<header className="">
-			<h1>This is the home</h1>
+			<h1>Welcome to Redpapr UI</h1>
+			<p>Here you can find the documentation for all Redpapr UI components.</p>
 		</header>
 	);
 }
