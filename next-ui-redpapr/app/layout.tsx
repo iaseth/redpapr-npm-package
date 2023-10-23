5add97065e7ddb8fb99163428c0eb0cf2a6e2150
Ankur Seth <iankurseth@gmail.com>
2023-10-23 11:25:02 +0530

    Added a css file to redpapr package

--- next-ui-redpapr/app/layout.tsx
+++ next-ui-redpapr/app/layout.tsx
@@ -1,5 +1,6 @@
 import RootLayout from '@/components/RootLayout/RootLayout';
 import './globals.css';
+import '../../package/css/redpapr.css';
 
 export const metadata = {
 	title: 'Redpapr',
--- next-ui-redpapr/app/page.tsx
+++ next-ui-redpapr/app/page.tsx
@@ -1,11 +1,10 @@
-import { H1 } from "../../package/src";
 
 
 
 export default function Home() {
 	return (
 		<header className="">
-			<H1>This is an H1</H1>
+			<h1>This is the home</h1>
 		</header>
 	);
 }
