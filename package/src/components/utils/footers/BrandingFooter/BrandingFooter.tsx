8309089a4c5021c9cbde490d091b5c3490dba1a4
Ankur Seth <iankurseth@gmail.com>
2023-10-24 09:13:48 +0530

    Added cool hover effect to SocialMediaLink

--- package/src/components/utils/footers/BrandingFooter/BrandingFooter.tsx
+++ package/src/components/utils/footers/BrandingFooter/BrandingFooter.tsx
@@ -6,22 +6,24 @@ import SocialMediaLink from "./SocialMediaLink";
 
 
 export default function BrandingFooter () {
+	const { meta } = redpaprJson;
 	return (
 		<footer className="bg-zinc-950 py-12 text-white text-left">
 			<section className="max-w-5xl mx-auto px-6 py-4 grid lg:grid-cols-3 gap-x-4 gap-y-6 text-zinc-300">
 				<section className="space-y-4">
 					<h2>Redpapr</h2>
 					<RedpaprIcon size={200} />
-					<h4>Doing the Tech bit in EdTech.</h4>
+					<h4>{meta.tagline}</h4>
+					<p>{meta.description}</p>
 				</section>
 
 				<section className="">
 					<h2>Contact Us</h2>
-					<section className="py-4 space-y-1">
+					<section className="py-4 space-y-1 text-zinc-400">
 						<p>You can call us at:</p>
-						<h4>{redpaprJson.meta.contactMobile}</h4>
+						<h4 className="text-zinc-200">{meta.contactMobile}</h4>
 						<p>You can Email us at:</p>
-						<h4>{redpaprJson.meta.contactEmail}</h4>
+						<h4 className="text-zinc-200">{meta.contactEmail}</h4>
 					</section>
 				</section>
 
--- package/src/components/utils/footers/BrandingFooter/SocialMediaLink.tsx
+++ package/src/components/utils/footers/BrandingFooter/SocialMediaLink.tsx
@@ -9,8 +9,14 @@ interface SocialMediaLinkProps {
 export default function SocialMediaLink ({link}: SocialMediaLinkProps) {
 	return (
 		<li className="text-zinc-400">
-			<h4 className="flex">
-				<a href={link.URL} target="_blank" className="py-1 red-link">{link.title} @{link.username}</a>
+			<h4 className="">
+				<a href={link.URL} target="_blank" className="block py-2 red-link flex items-stretch border border-transparent rounded hover:px-4 hover:bg-zinc-900 hover:border-red-500">
+					<span>{link.title}</span>
+					<span className="grow flex px-4 pt-1">
+						<span className="my-auto block h-1 bg-red-500 w-full"></span>
+					</span>
+					<span>@{link.username}</span>
+				</a>
 			</h4>
 		</li>
 	);
