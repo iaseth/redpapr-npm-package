cbfa5dcbe5fa3ed8fc19e10a4f6eea969db1f0bc
Ankur Seth <iankurseth@gmail.com>
2024-08-21 11:44:07 +0530

    Now importing SCSS code directly into UI

--- next-ui-redpapr/app/layout.tsx
+++ next-ui-redpapr/app/layout.tsx
@@ -1,6 +1,8 @@
 import RootLayout from '@/components/RootLayout/RootLayout';
 import './globals.css';
-import '../../node/css/redpapr.css';
+import '../../node/scss/redpapr.scss';
+
+
 
 export const metadata = {
 	title: 'Red UI',
