8ada1d760148b1f50c28aec4bfd011043050f824
Ankur Seth <iankurseth@gmail.com>
2024-09-05 11:15:12 +0530

    Corrected SocialMediaLink and MobileNumber

--- node/src/components/base/MobileNumber.tsx
+++ node/src/components/base/MobileNumber.tsx
@@ -2,14 +2,16 @@
 
 
 interface MobileNumberProps {
-	mobileNumber: string
+	mobileNumber: string,
+	countryCode?: string
 }
 
 export default function MobileNumber ({
-	mobileNumber
+	mobileNumber, countryCode="+91"
 }: MobileNumberProps) {
 	return (
-		<span className="red-link space-x-1">
+		<span className="red-link space-x-1.5">
+			<span>{countryCode}</span>
 			<span>{mobileNumber.slice(0, 3)}</span>
 			<span>{mobileNumber.slice(3, 6)}</span>
 			<span>{mobileNumber.slice(6)}</span>
